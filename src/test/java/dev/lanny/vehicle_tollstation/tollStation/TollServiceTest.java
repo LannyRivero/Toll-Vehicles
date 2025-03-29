ee78215dcc2d3ecd61286fb11e3dac0f280a21b3
lannyRivero <lannyrivero288@gmail.com>
2025-03-30 00:49:47 +0100

    Add failing test for retrieving TollStation by ID(RED)

---
+++ src/test/java/dev/lanny/vehicle_tollstation/tollStation/TollServiceTest.java
@@ -0,0 +1,24 @@
+package dev.lanny.vehicle_tollstation.tollStation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TollServiceTest {
+
+    private TollService tollService = new TollService();
+
+    @Test
+    @DisplayName("Validate return TollStation by ID")
+    void test_Should_Return_TollStation_By_Id() {
+        TollStation station = new TollStation("Test Station", "Valencia");
+        tollService.addTollStation(42L, station); 
+        TollStation result = tollService.getTollStation(42L);
+
+        assertNotNull(result);
+        assertEquals("Test Station", result.getName());
+        assertEquals("Valencia", result.getCity());
+    }
+
+}
