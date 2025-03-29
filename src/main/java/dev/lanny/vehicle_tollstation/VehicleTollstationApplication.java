c29e0a4ff25d02977350fcf441be33fc1464f653
lannyRivero <lannyrivero288@gmail.com>
2025-03-29 11:17:40 +0100

    first commit

---
+++ src/main/java/dev/lanny/vehicle_tollstation/VehicleTollstationApplication.java
@@ -0,0 +1,13 @@
+package dev.lanny.vehicle_tollstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleTollstationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleTollstationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lanny/vehicle_tollstation/VehicleTollstationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lanny.vehicle_tollstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleTollstationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
