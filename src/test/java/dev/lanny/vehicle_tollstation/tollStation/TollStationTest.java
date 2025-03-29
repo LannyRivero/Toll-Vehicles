0065cea68cccb3900501d1041613663e048bc68e
lannyRivero <lannyrivero288@gmail.com>
2025-03-30 00:02:55 +0100

    Add test for addVehicle method in TollStation(RED)

--- src/test/java/dev/lanny/vehicle_tollstation/tollStation/TollStationTest.java
+++ src/test/java/dev/lanny/vehicle_tollstation/tollStation/TollStationTest.java
@@ -7,6 +7,9 @@ import java.math.BigDecimal;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import dev.lanny.vehicle_tollstation.vehicle.Vehicle;
+import dev.lanny.vehicle_tollstation.vehicle.VehicleType;
+
 public class TollStationTest {
 
     @Test
@@ -32,6 +35,18 @@ public class TollStationTest {
         assertEquals("New Station", station.getName());
         assertEquals("Barcelona", station.getCity());
         assertEquals(new BigDecimal("250.00"), station.getTotalCollected());
+    }
+
+    @Test
+    @DisplayName("Validate add a vehicle to the registered vehicles list")
+    void test_Should_Add_Vehicle_To_Registered_Vehicles() {
+        TollStation station = new TollStation("Station A", "Madrid");
+
+        Vehicle vehicle = new Vehicle("ABC123", new BigDecimal("100.00"), VehicleType.CAR);
+        station.addVehicle(vehicle);
+
+        assertEquals(1, station.getRegisteredVehicles().size());
+        assertEquals("ABC123", station.getRegisteredVehicles().get(0).getLicensePlate());
     }
 
 }
