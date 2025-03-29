a04ffc5fceb23e4c067f8596898fae1234c59068
lannyRivero <lannyrivero288@gmail.com>
2025-03-30 00:52:16 +0100

    Implement  beforeEach to test

--- src/test/java/dev/lanny/vehicle_tollstation/tollStation/TollServiceTest.java
+++ src/test/java/dev/lanny/vehicle_tollstation/tollStation/TollServiceTest.java
@@ -2,6 +2,7 @@ package dev.lanny.vehicle_tollstation.tollStation;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,11 +10,18 @@ public class TollServiceTest {
 
     private TollService tollService = new TollService();
 
+    @BeforeEach
+    void setUp() {
+        tollService = new TollService();
+        TollStation station = new TollStation("Main Station", "Seville");
+        tollService.addTollStation(1L, station);
+    }
+
     @Test
     @DisplayName("Validate return TollStation by ID")
     void test_Should_Return_TollStation_By_Id() {
         TollStation station = new TollStation("Test Station", "Valencia");
-        tollService.addTollStation(42L, station); 
+        tollService.addTollStation(42L, station);
         TollStation result = tollService.getTollStation(42L);
 
         assertNotNull(result);
