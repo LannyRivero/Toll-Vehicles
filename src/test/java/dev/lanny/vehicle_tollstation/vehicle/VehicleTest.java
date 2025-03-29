81fe00790c18740394d46efc52205b2f9bb260be
lannyRivero <lannyrivero288@gmail.com>
2025-03-29 23:24:56 +0100

    Add failing test to verify Vehicle setters(RED)

--- src/test/java/dev/lanny/vehicle_tollstation/vehicle/VehicleTest.java
+++ src/test/java/dev/lanny/vehicle_tollstation/vehicle/VehicleTest.java
@@ -4,16 +4,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.math.BigDecimal;
 
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class VehicleTest {
 
-     @Test
+    @Test
     @DisplayName("Validate create a Vehicle with license plate, VehicleType and toll amount")
     void test_Should_Create_Vehicle_With_Fields() {
-        
+
         VehicleType type = VehicleType.CAR;
         String licensePlate = "ABC123";
         BigDecimal tollAmount = new BigDecimal("100.00");
@@ -23,6 +22,19 @@ public class VehicleTest {
         assertEquals(licensePlate, vehicle.getLicensePlate());
         assertEquals(tollAmount, vehicle.getTollAmount());
         assertEquals(type, vehicle.getType());
+    }
+
+    @Test
+    @DisplayName("Validate allow updating vehicle fields through setters")
+    void test_Should_Update_Vehicle_Fields() {
+        Vehicle vehicle = new Vehicle();
+        vehicle.setLicensePlate("ZZZ999");
+        vehicle.setTollAmount(new BigDecimal("75.00"));
+        vehicle.setType(VehicleType.TRUCK);
+
+        assertEquals("ZZZ999", vehicle.getLicensePlate());
+        assertEquals(new BigDecimal("75.00"), vehicle.getTollAmount());
+        assertEquals(VehicleType.TRUCK, vehicle.getType());
     }
 
 }
