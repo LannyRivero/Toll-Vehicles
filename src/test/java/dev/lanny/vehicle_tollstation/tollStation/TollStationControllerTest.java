d905d1e7a1909366af587de0cbef9d3a2ce51046
lannyRivero <lannyrivero288@gmail.com>
2025-03-31 22:14:43 +0200

    Add failing test for GET /report to return toll summary(RED)

--- src/test/java/dev/lanny/vehicle_tollstation/tollStation/TollStationControllerTest.java
+++ src/test/java/dev/lanny/vehicle_tollstation/tollStation/TollStationControllerTest.java
@@ -4,12 +4,15 @@ import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import static org.hamcrest.Matchers.containsString;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.MediaType;
@@ -25,6 +28,7 @@ import dev.lanny.vehicle_tollstation.vehicle.VehicleType;
 public class TollStationControllerTest {
     private MockMvc mockMvc;
     private Vehicle vehicle;
+    private Vehicle vehicle1;
 
     @Mock
     private TollStation tollStation;
@@ -37,12 +41,13 @@ public class TollStationControllerTest {
         MockitoAnnotations.openMocks(this);
         mockMvc = MockMvcBuilders.standaloneSetup(tollStationController).build();
         vehicle = new Vehicle("ABC123", new BigDecimal("100.00"), VehicleType.CAR);
+        vehicle1 = new Vehicle("TCC123", new BigDecimal("100.00"), VehicleType.TRUCK);
     }
 
     @Test
     @DisplayName("Validate collect toll from vehicle and return 200 OK")
     void test_Should_Collect_Toll_Successfully() throws Exception {
-       
+
         ObjectMapper mapper = new ObjectMapper();
 
         mockMvc.perform(post("/toll")
@@ -51,6 +56,21 @@ public class TollStationControllerTest {
                 .andExpect(status().isOk());
 
         verify(tollStation, times(1)).addVehicle(any());
+    }
+
+    @Test
+    @DisplayName("VAlidate return report with vehicles and total collected")
+    void test_Should_Return_Report() throws Exception {
+        when(tollStation.getName()).thenReturn("Main");
+        when(tollStation.getCity()).thenReturn("Seville");
+        when(tollStation.getRegisteredVehicles()).thenReturn(List.of(vehicle, vehicle1));
+               
+             
+        mockMvc.perform(get("/report"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Main")))
+                .andExpect(content().string(containsString("TCC123")))
+                .andExpect(content().string(containsString("Total recolectado")));
     }
 
 }
